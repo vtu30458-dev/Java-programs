e846f85d2f9aeb9bd116223dd09e8df773402e1f
Balaiah Gari Vasu <vtu30458@veltech.edu.in>
2026-07-23 10:35:51 +0530

    Add files via upload

---
+++ array elements.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ArrayIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index < n) {
+            System.out.println("Element = " + arr[index]);
+        } else {
+            System.out.println("Invalid Index");
+        }
+    }
+}
+
+Output:
+Enter size: 5
+Enter sorted array:
+10
+20
+30
+40
+50
+Enter element to search: 40
