75ffbff4424b17d77562929e1eb8b96c32b3d8bd
Balaiah Gari Vasu <vtu30458@veltech.edu.in>
2026-07-30 10:10:33 +0530

    Add files via upload

---
+++ Build  array from permutation task.1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+
+Output:
+[0,1,2,4,5,3]
