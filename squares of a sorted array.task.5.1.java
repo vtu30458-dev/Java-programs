5c1d87c4f6293bdb84a6c9a0f4b42be4f4d8b616
Balaiah Gari Vasu <vtu30458@veltech.edu.in>
2026-07-30 23:22:07 +0530

    Add files via upload

---
+++ squares of a sorted array.task.5.1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
+
+Output:
+nums =
+[-4,-1,0,3,10]
+[0,1,9,16,100]
