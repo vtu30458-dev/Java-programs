5b7493d7c3edb62992b6b07d498d1c5eb9321a93
Balaiah Gari Vasu <vtu30458@veltech.edu.in>
2026-07-30 23:26:29 +0530

    Add files via upload

---
+++ find pivot index.task.5.2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int n : nums) {
+            total += n;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
+
+Output:
+nums =
+[1,7,3,6,5,6]
+3
